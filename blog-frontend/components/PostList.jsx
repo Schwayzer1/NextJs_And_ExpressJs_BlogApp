14bb196f2a973ba6cd9dd58a2678434c8280f51a
Schwayzer <furkann.onay@gmail.com>
2023-05-03 15:10:37 +0300

    .

--- blog-frontend/components/PostList.jsx
+++ blog-frontend/components/PostList.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const PostList = ({ data, setData }) => {
   return (
-    <section class="container mx-auto p-5 md:p-10 grid lg:grid-cols-2 2xl:grid-cols-3 grid-cols-1 gap-y-10 transform duration-500 mb-5 text-center justify-center flex">
+    <section class="container mx-auto p-5 md:p-10 grid lg:grid-cols-2 2xl:grid-cols-3 grid-cols-1 gap-y-10 transform duration-500 mb-5 text-center justify-center">
       <Card data={data} setData={setData} />
     </section>
   );
